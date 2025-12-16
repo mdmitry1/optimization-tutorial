035d7cd0bbf2f813428a9ca0fb65a2ed9dba1a48
Dmitry Messerman <dmitry.messerman@gmail.com>
2025-12-16 18:58:15 +0200

    Fixed test name

--- tutorial/examples/constraint_dora/py/test_slsqp.py
+++ tutorial/examples/constraint_dora/py/test_slsqp.py
@@ -3,7 +3,7 @@ import sys
 from constraint_dora import main
 from os.path import exists
 
-def test_optimization_ex(monkeypatch):
+def test_constraint_dora(monkeypatch):
     with monkeypatch.context() as m:
         m.setattr(sys, 'argv', ['constraint_dora'])
         assert main() == 'a757d8ff6f40eacdbc7de3dd6c8afae2041f9bc2c7cd706ee0f0660f59d33803'
