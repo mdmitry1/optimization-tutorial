33accafb93aca69d29611359c84277a7e09c52ea
Dmitry Messerman <dmitry.messerman@gmail.com>
2025-12-23 11:31:58 +0200

    Added dataset generation

--- tutorial/examples/bnh/py/test_nsga2.py
+++ tutorial/examples/bnh/py/test_nsga2.py
@@ -10,9 +10,11 @@ def test_nsga2(monkeypatch, request):
     root_dir = str(request.config.rootpath) + '/'
     with monkeypatch.context() as m:
         test_path = dirname(realpath(root_dir + getenv('PYTEST_CURRENT_TEST').split(':')[0]))
-        out = test_path + '/NSGA2_pareto.txt'
-        if exists(out):
-            remove(out)
+        out  = test_path + '/NSGA2_pareto.txt'
+        out1 = test_path + '/dataset.csv'
+        for o in [out, out1]:
+            if exists(out):
+                remove(out)
         assert exists(out) == False
         print("")
         m.setattr(sys, 'argv', ['bnh'])
@@ -21,3 +23,4 @@ def test_nsga2(monkeypatch, request):
             assert int(popen(f"sum {out}").read().split()[0]) == 19751
         else:
             assert int(popen(f"sum {out}").read().split()[0]) == 43431
+        assert int(popen(f"sum {out1}").read().split()[0]) == 9816
