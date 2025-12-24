f7acbf495cd99e9268b6180171d7ce541f69530b
Dmitry Messerman <dmitry.messerman@gmail.com>
2025-12-24 16:34:41 +0200

    Added decision tree test

--- tutorial/examples/pyomo/py/test_bnh_csv_decision_tree.py
+++ tutorial/examples/pyomo/py/test_bnh_csv_decision_tree.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3.12
 import sys
-from bnh_csv import main
+from bnh_csv_decision_tree import main
 from os import remove, popen
 from os.path import exists, realpath, dirname
 from sys import version
 from os import getenv
 
-def test_bnh_csv(monkeypatch, request):
+def test_bnh_csv_decision_tree(monkeypatch, request):
     root_dir = str(request.config.rootpath) + '/'
     with monkeypatch.context() as m:
         test_path = dirname(realpath(root_dir + getenv('PYTEST_CURRENT_TEST').split(':')[0]))
         print("")
         m.setattr(sys, 'argv', ['bnh_csv'])
         if version.split()[0] == '3.14.2':
-            assert main(test_path) == "8e5f3eeefd9919cf27e458765f3b5a58661d21d83a49b47cbdc802a0d4bf17e8"
+            assert main(test_path) == "8a17cb976a0b670c6b874201065dfca9bec12dc5eb879b49dc2d356ceb4c4f0f"
         else:
-            assert main(test_path) == "9fdec865e276c939aa69b5f677702e3ebf3aae4919d1bd374d2e84c49f17ddd7"
+            assert main(test_path) == "394e9980b014c68cf201eedbbe85ef74251a7f3ad4d9ad7ce0f1db86b85cb0a0"
