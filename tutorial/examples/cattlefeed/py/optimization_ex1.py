3011fcf97c17a12b14b80b063f966b2e360582d3
Dmitry Messerman <dmitry.messerman@gmail.com>
2025-12-26 15:49:57 +0200

    Refactoring

--- tutorial/examples/cattlefeed/py/optimization_ex1.py
+++ tutorial/examples/cattlefeed/py/optimization_ex1.py
@@ -1,30 +1,22 @@
 #!/usr/bin/python3.14
 # Reference: https://shgo.readthedocs.io/en/latest/docs/README.html#1-endres-sc--sandrock-c-focke-ww-2018-a-simplicial-homology-algorithm-for-lipschitz-optimisation-journal-of-global-optimization
 from shgo import shgo
-from numpy import sqrt
+from numpy import sqrt, dot, sum
 from hashlib import sha256
 
-def f(x):  # (cattle-feed)
-    return 24.55*x[0] + 26.75*x[1] + 39*x[2] + 40.50*x[3]
-
-def g1(x):
-    return 2.3*x[0] + 5.6*x[1] + 11.1*x[2] + 1.3*x[3] - 5  # >=0
-
-def g2(x):
-    return (12*x[0] + 11.9*x[1] +41.8*x[2] + 52.1*x[3] - 21
-            - 1.645 * sqrt(0.28*x[0]**2 + 0.19*x[1]**2
-                         + 20.5*x[2]**2 + 0.62*x[3]**2)
-            ) # >=0
-
-def h1(x):
-    return x[0] + x[1] + x[2] + x[3] - 1  # == 0
-
 def main():
+    v=[[24.55,26.75,39,40.50],[2.3,5.6,11.1,1.3],
+       [12,11.9,41.8,52.1],[0.28,0.19,20.5,0.62]]
+
+    g1 = lambda x: dot(x,v[1]) - 5
+    g2 = lambda x: dot(x,v[2]) - 1.645 * sqrt(dot([x**2 for x in x],v[3])) - 21
+    h1 = lambda x: sum(x) - 1
+
     cons = ({'type': 'ineq', 'fun': g1},
             {'type': 'ineq', 'fun': g2},
-            {'type': 'eq', 'fun': h1})
-    bounds = [(0, 1.0),]*4
-    res = shgo(f, bounds, iters=3, constraints=cons)
+            {'type': 'eq',   'fun': h1})
+    #shgo(f, bounds, iters, constraints)
+    res = shgo(lambda x: dot(x,v[0]), [(0, 1.0),]*4, iters=3, constraints=cons)
     pprint_res=f'x1={res.x[0]:.3f},x2={res.x[1]:.3f},x3={res.x[2]:.3f},x4={res.x[3]:.3f},f={res.fun:.3f}\n' + \
                f'g1={g1(res.x):.3f},g2={g2(res.x):.3f},g3={h1(res.x):.3f}'
     print(pprint_res)
