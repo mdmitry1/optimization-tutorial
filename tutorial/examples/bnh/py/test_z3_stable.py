5bc0b14f96e2c9f40feea577fc5d0a32283228f6
Dmitry Messerman <dmitry.messerman@gmail.com>
2025-12-30 16:16:25 +0200

    Added yet another signature

--- tutorial/examples/bnh/py/test_z3_stable.py
+++ tutorial/examples/bnh/py/test_z3_stable.py
@@ -13,4 +13,5 @@ def test_z3_stable(monkeypatch, request):
         m.setattr(sys, 'argv', ['bnh_z3'])
         result = solve_bnh_z3(test_path)
         assert result == "f6837c61792b36166b0394286ad5da8e89e334b392e3aec4ffa9e02fa40f0e70" or \
-               result == "08af985a9d077117f4bdc8240c0d4bdeec24265796b51162e4edf526b324c03d"
+               result == "08af985a9d077117f4bdc8240c0d4bdeec24265796b51162e4edf526b324c03d" or \
+               result == "b101db1cba193f3dfbfcc5282fbc23a5a55d19e67916ab2f83d6fdbbccbea394"
