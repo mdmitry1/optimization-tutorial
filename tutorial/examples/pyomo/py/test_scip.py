5177551c3437eb1e19223e80997dced171e41ffd
Dmitry Messerman <dmitry.messerman@gmail.com>
2026-01-23 11:41:12 +0200

    Added new signature after software reinstallation

--- tutorial/examples/pyomo/py/test_scip.py
+++ tutorial/examples/pyomo/py/test_scip.py
@@ -11,4 +11,6 @@ def test_scip(monkeypatch, request):
         test_path = dirname(realpath(root_dir + getenv('PYTEST_CURRENT_TEST').split(':')[0]))
         print("")
         m.setattr(sys, 'argv', ['scip'])
-        assert main(test_path,5000) == "63af7a9f330083a8164db242198801fd756f2e94e19485ec245144b000f69a99"
+        result = main(test_path,5000)
+        assert result == "63af7a9f330083a8164db242198801fd756f2e94e19485ec245144b000f69a99" or \
+               result == '3b8da2b87a4908fa4d7a966c5b44d516b806e7ee1c474cfb1f8bf215365611e0'
