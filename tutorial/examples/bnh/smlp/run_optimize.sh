e0a7d94c4cc844c277f5e9e07f4b4923663db58e
Dmitry Messerman <dmitry.messerman@gmail.com>
2026-03-13 09:52:34 +0200

    Optimized performance

--- tutorial/examples/bnh/smlp/run_optimize.sh
+++ tutorial/examples/bnh/smlp/run_optimize.sh
@@ -26,35 +26,34 @@ o4="(-(F1*0.4+F2*0.6))"
 o5="(-(F1*0.2+F2*0.8))"
 o6="(-F2)"
 
-objs=("$o1" "$o2" "$o3" "$o4" "$o5" "$o6")
+python3.11 "$(realpath "$0" | xargs dirname | xargs dirname | xargs dirname | xargs dirname | xargs dirname)/src/run_smlp.py" \
+    -data "$csv" \
+    -spec "$json" \
+    -out_dir ./ \
+    -pref "$test" \
+    -mode optimize \
+    -pareto f \
+    -opt_strategy eager \
+    -resp F1,F2 \
+    -feat X1,X2 \
+    -objv_names "w1,w2,w3,w4,w5,w6" \
+    -objv_exprs "$o1;$o2;$o3;$o4;$o5;$o6" \
+    -model poly_sklearn \
+    -tree_encoding flat \
+    -compress_rules t \
+    -mrmr_pred 0 \
+    -epsilon 0.000005 \
+    -delta_rel 0.05 \
+    -save_model t \
+    -model_name "${csv%.*}_model" \
+    -save_model_config t \
+    -plots f \
+    -pred_plots f \
+    -resp_plots f \
+    -seed 10 \
+    -log_time f > "$(basename "$0").log" 2>&1
 
-n=1
-for obj in "$o1" "$o2" "$o3" "$o4" "$o5" "$o6"; do
-    python3.11 "$(realpath "$0" | xargs dirname | xargs dirname | xargs dirname | xargs dirname | xargs dirname)/src/run_smlp.py" \
-        -data "$csv" \
-        -spec "$json" \
-        -out_dir ./ \
-        -pref "$test" \
-        -mode optimize \
-        -pareto f \
-        -opt_strategy eager \
-        -resp F1,F2 \
-        -feat X1,X2 \
-        -objv_exprs "$obj" \
-        -model poly_sklearn \
-        -tree_encoding flat \
-        -compress_rules t \
-        -mrmr_pred 0 \
-        -epsilon 0.000005 \
-        -delta_rel 0.05 \
-        -save_model t \
-        -model_name "${csv%.*}_model" \
-        -save_model_config t \
-        -plots f \
-        -pred_plots f \
-        -resp_plots f \
-        -seed 10 \
-        -log_time f > "$(basename "$0").log" 2>&1
-    mv -f "BNH_BNH_optimization_results.json" "${test_lc}_p${n}_optimization_results.json"
-    (( n++ ))
+for var in "X1" "X2" "F1" "F2"; do 
+    jq --arg f "$var" '.[] | select(type == "object") | .[$f]' ${test}_${test}_optimization_results.json  | \
+        sed 's/E/e/g' > ${test_lc}_pareto_${var}.txt
 done
