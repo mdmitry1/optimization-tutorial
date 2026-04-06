1fdd1a75bf67564ac24f05b0073f3098c22b6ab6
Dmitry Messerman <dmitry.messerman@gmail.com>
2026-04-06 17:58:48 +0300

    Removed smlp and SMLP from the repository

--- tutorial/examples/pyomo/py/generated_constraints_claude_expected.py
+++ tutorial/examples/pyomo/py/generated_constraints_claude_expected.py
@@ -1,7 +1,7 @@
 # ============================================================================
 # GENERATED CONSTRAINT FUNCTIONS FROM JSON ALPHA ATTRIBUTE
 # ============================================================================
-# Source: /home/mdmitry/github/smlp_tutorial/tutorial/examples/pyomo/py/bnh.json
+# Source: /home/mdmitry/github/optimization_tutorial/tutorial/examples/pyomo/py/bnh.json
 # Variables: X1, X2
 # Alpha: (X1-5)*(X1-5)+X2*X2-25 and -(X1-8)*(X1-8)-(X2+3)*(X2+3)+7.7
 # Number of constraints: 2
