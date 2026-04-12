0dd91bb74a2856ff47904e3bf35df63b7cb4c3c3
Dmitry Messerman <dmitry.messerman@gmail.com>
2026-04-12 12:24:04 +0300

    Added WSL2 signatures for test_gpsampler.py and test_keras.py

--- optexamples/examples/shekel/py/test_keras.py
+++ optexamples/examples/shekel/py/test_keras.py
@@ -14,4 +14,6 @@ def test_keras(monkeypatch, request):
         test_path = dirname(realpath(root_dir + getenv('PYTEST_CURRENT_TEST').split(':')[0]))
         print("")
         m.setattr(sys, 'argv', ['keras_ex'])
-        assert main(512, test_path) == '3be173406872257840c43d3c0a15f1115a87dd1145eac802b893832501ea3b60'
+        result = main(512, test_path)
+        assert result == '3be173406872257840c43d3c0a15f1115a87dd1145eac802b893832501ea3b60' or \
+               result == 'd353c09d5636cc3ca3e92fd0f2d35da421d6755d9708db8fb819639661574589'
